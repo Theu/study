7391d5008df29effb45be363a1c70396475653f3
Matteo Contini <matteo.contini@zalando-lounge.de>
2017-06-08 17:56:29 +0200

    mO reverte clg, refactor

---
+++ functionBase/rewrite.js
@@ -0,0 +1,12 @@
+// function checkAge(age) {
+//   if (age > 18) {
+//     return true;
+//   } else {
+//     return confirm('Do you have your parents permission to access this page?');
+//   }
+// }
+
+// let checkAge = (age) => (age > 18) ? true : confirm('Do you have your parents permission to access this page KK?');
+
+let checkAge = (age) => (age > 18) || confirm('Did parents dd you?');
+
